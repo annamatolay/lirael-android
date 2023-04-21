323da9225e61570f80138407c863d8e8d9aefcfd
Anna Matolay <anmatolay@gmail.com>
2023-04-21 19:00:37 +0200

    fix: image loading on random recipe cards

--- app/src/main/java/dev/anmatolay/lirael/presentation/statistics/RandomRecipeAdapter.kt
+++ app/src/main/java/dev/anmatolay/lirael/presentation/statistics/RandomRecipeAdapter.kt
@@ -1,8 +1,8 @@
 package dev.anmatolay.lirael.presentation.statistics
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import dev.anmatolay.lirael.R
@@ -11,31 +11,30 @@ import dev.anmatolay.lirael.domain.model.Recipe
 
 class RandomRecipeAdapter(private val dataSet: List<Recipe>) : RecyclerView.Adapter<RandomRecipeAdapter.ViewHolder>() {
 
-    private lateinit var binding: LayoutItemRandomRecipeBinding
-
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    class ViewHolder(val binding: LayoutItemRandomRecipeBinding) : RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        binding = LayoutItemRandomRecipeBinding.inflate(LayoutInflater.from(parent.context))
-        return ViewHolder(binding.root)
+        val binding = LayoutItemRandomRecipeBinding.inflate(LayoutInflater.from(parent.context))
+        return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val recipe = dataSet[position]
         val context = holder.itemView.context
-        recipe.run {
+        with(holder) {
             binding.title.text = recipe.title
             binding.ingredientsCount.text =
                 context.getString(R.string.recipe_card_ingredients, recipe.ingredients.size)
             binding.instructionsCount.text =
                 context.getString(R.string.recipe_card_ingredients, recipe.ingredients.size)
             val image = recipe.imageUrl ?: R.drawable.cat_error_not_found
-            // FIXME : img not loading
             binding.image.load(image)
         }
-
+        holder.itemView.setOnClickListener {
+            // TODO: start cooking flow
+            Toast.makeText(holder.itemView.context, "Coming soon", Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun getItemCount(): Int = dataSet.size
-
 }
