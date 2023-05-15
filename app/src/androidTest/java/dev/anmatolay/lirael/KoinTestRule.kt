c74adcebdba3930c4255372736ccd15e1546016d
Anna Matolay <anmatolay@gmail.com>
2023-05-16 00:02:30 +0200

    chore: inject fakes and mock firebase

--- app/src/androidTest/java/dev/anmatolay/lirael/KoinTestRule.kt
+++ app/src/androidTest/java/dev/anmatolay/lirael/KoinTestRule.kt
@@ -1,13 +1,26 @@
 package dev.anmatolay.lirael
 
 import androidx.test.platform.app.InstrumentationRegistry
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.crashlytics.FirebaseCrashlytics
+import dev.anmatolay.lirael.core.analytics.AnalyticsWrapper
+import dev.anmatolay.lirael.core.analytics.impl.FakeAnalyticsImpl
+import dev.anmatolay.lirael.core.authentication.Authenticator
+import dev.anmatolay.lirael.core.authentication.impl.FakeAuthenticatorImpl
 import dev.anmatolay.lirael.core.di.module.*
+import dev.anmatolay.lirael.core.threading.SchedulerProvider
+import dev.anmatolay.lirael.core.threading.impl.TestSchedulerProvider
+import dev.anmatolay.lirael.util.MockUserPropertyImpl
+import dev.anmatolay.lirael.util.UserProperty
+import io.mockk.mockk
 import org.junit.rules.TestWatcher
 import org.junit.runner.Description
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 import org.koin.core.context.stopKoin
 import org.koin.core.module.Module
+import org.koin.dsl.module
 
 class KoinTestRule(
     private val modules: List<Module>
@@ -21,12 +34,21 @@ class KoinTestRule(
             )
             modules(
                 appModule,
-                firebaseModule,
                 viewModelModule,
                 repositoryModule,
                 useCaseModule,
                 dataSourceModule,
+                databaseModule,
             )
+            module {
+                factory<SchedulerProvider> { TestSchedulerProvider() }
+                factory<Authenticator> { FakeAuthenticatorImpl() }
+                factory<AnalyticsWrapper> { FakeAnalyticsImpl() }
+                factory<UserProperty> { MockUserPropertyImpl }
+                factory<FirebaseAnalytics> { mockk(relaxed = true) }
+                factory<FirebaseCrashlytics> { mockk(relaxed = true) }
+                factory<FirebaseAuth> { mockk(relaxed = true) }
+            }
             modules(modules)
         }
     }
