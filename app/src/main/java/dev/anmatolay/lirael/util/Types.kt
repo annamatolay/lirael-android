54ee7d9eb4e1e75599298be87f9acf710d14a37f
Anna Matolay <anmatolay@gmail.com>
2023-05-15 17:51:14 +0200

    chore: fixed doc

--- app/src/main/java/dev/anmatolay/lirael/util/Types.kt
+++ app/src/main/java/dev/anmatolay/lirael/util/Types.kt
@@ -1,6 +1,10 @@
 package dev.anmatolay.lirael.util
 
-// String: formatted DateTime (LLLL yyyy) [@see Constants#getDefaultDateTimeFormatter]
-// Long: Epoch DateTime within the month of year related to the String key
-// Int: Times of cooking on the given day (related to the Long key)
+/**
+ * Map representation of [@see RecipeHistory]
+ *
+ * String: formatted DateTime (LLLL yyyy) [@see LocalDateProvider#getDefaultDateTimeFormatter]
+ * Long: Epoch DateTime within the month of year related to the String key
+ * Int: Times of cooking on the given day (related to the Long key)
+ */
 typealias CookingHistoryMap = Map<String, Map<Long, Int>>
